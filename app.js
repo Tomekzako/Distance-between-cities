934c0ef96f80857702db8e3003cb0fcd65e99901
Tomasz Chyc <tomek.chyc55@gmail.com>
2017-07-29 13:09:17 +0200

    creating google directionsService and Display

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-    var map;
     var london = {
-                lat: 51.5,
-                lng: -0.1        
+        lat: 51.5,
+        lng: -0.1
     }
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: london,
-            zoom: 8,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+
+    var mapOptions = {
+        center: london,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var directionsService = new google.maps.DirectionsService();
+
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
+    directionsDisplay.setMap(map);
+
+    function calcRoute() {
+        var request = {
+            origin: document.getElementById('from').value,
+            destination: document.getElementById('to').value,
+            travelMode: google.maps.TravelMode.DRIVING,
+            unitSystem: google.maps.UnitSystem.METRIC
+        }
+        directionsService.route(request, function (result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                console.log(result);
+                $(".info").html("<div class='alert-info'>From: " + document.getElementById('from').value + ".<br/>To: " + document.getElementById('to').value + ".<br/>Driving distance: " + result.routes[0].legs[0].distance.text + ".<br/>Duration: " + result.routes[0].legs[0].duration.text + "</div>");
+
+                directionsDisplay.setDirections(result);
+            } else {
+                directionsDisplay.setDirections({
+                    routes: []
+                });
+                map.setCenter(london);
+                $(".info").html("<div class='alert-danger'>Could not retrieve driving distance.</div>");
+            }
+
         });
     }
